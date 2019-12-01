67f3753b165c7db37916ea93d4e39c1b40851a38
Jesus Gomez Bellido <jesus_jgb_45@hotmail.com>
2019-12-01 13:20:02 +0100

    refactor: split in files

---
+++ Day-01/dreidel-symbol.ts
@@ -0,0 +1,23 @@
+interface IDreidelSymbol {
+  code: string;
+  name: string;
+}
+
+export const faces: IDreidelSymbol[] = [
+  {
+    code: "נ",
+    name: "Nun"
+  },
+  {
+    code: "ג",
+    name: "Gimmel"
+  },
+  {
+    code: "ה",
+    name: "Hay"
+  },
+  {
+    code: "ש",
+    name: "Shin"
+  }
+];
--- Day-01/index.ts
+++ Day-01/index.ts
@@ -1,32 +1,9 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
-interface IDreidelSymbol {
-  code: string;
-  name: string;
-}
-
-const faces: IDreidelSymbol[] = [
-  {
-    code: "נ",
-    name: "Nun"
-  },
-  {
-    code: "ג",
-    name: "Gimmel"
-  },
-  {
-    code: "ה",
-    name: "Hay"
-  },
-  {
-    code: "ש",
-    name: "Shin"
-  }
-];
+import { faces } from './dreidel-symbol';
 
 const httpTrigger: AzureFunction = async function(
-  context: Context,
-  req: HttpRequest
+  context: Context
 ): Promise<void> {
   const faceIndex = Math.floor(Math.random() * 4);
   const face = faces[faceIndex];
